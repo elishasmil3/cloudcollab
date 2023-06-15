1a6604fafb3f1cd2de1953c1c9abfe087d7ddc63
Elisha Smile <91423779+Yaar-ada-yaar@users.noreply.github.com>
2023-06-16 00:09:07 +0530

    basicComponents, Contains Plans Component

--- assets/js/mobNav.js
+++ assets/js/mobNav.js
@@ -1,10 +1,17 @@
 var nav = 1;
 function mobNavToggle() {
-  if ((nav == 1)) {
+  if (nav == 1) {
     document.getElementById("mobNavLinks").style.display = "flex";
     return (nav = 0);
-} else if(nav == 0){
+  } else if (nav == 0) {
     document.getElementById("mobNavLinks").style.display = "none";
     return (nav = 1);
   }
 }
+
+// function mobNavHide() {
+//   setTimeout(() => {
+//     document.getElementById("mobNavLinks").style.display = "none";
+//   }, 1000);
+
+// }
