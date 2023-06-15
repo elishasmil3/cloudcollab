c866c2c08a19231eb927da8832864fb3851f26ec
Elisha Smile <91423779+elishasmil3@users.noreply.github.com>
2023-06-15 19:56:10 +0530

    Initial Uploads

---
+++ assets/js/mobNav.js
@@ -0,0 +1,10 @@
+var nav = 1;
+function mobNavToggle() {
+  if ((nav == 1)) {
+    document.getElementById("mobNavLinks").style.display = "flex";
+    return (nav = 0);
+} else if(nav == 0){
+    document.getElementById("mobNavLinks").style.display = "none";
+    return (nav = 1);
+  }
+}
