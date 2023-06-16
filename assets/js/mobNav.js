aef8b2bd5441dde4c979345bbee336a021010cf3
Elisha Smile <91423779+Yaar-ada-yaar@users.noreply.github.com>
2023-06-17 02:24:37 +0530

    completeDesign

--- assets/js/mobNav.js
+++ assets/js/mobNav.js
@@ -1,10 +1,25 @@
 var nav = 1;
+var cssLink = document.createElement("link");      if (cssLink) {
+  document.head.removeChild(cssLink);
+  cssLink = null;
+}
 function mobNavToggle() {
   if (nav == 1) {
     document.getElementById("mobNavLinks").style.display = "flex";
+
+    cssLink.rel = "stylesheet";
+    cssLink.href = "./assets/css/re.css";
+    cssLink.type = "text/css";
+
+    document.head.appendChild(cssLink);
+
     return (nav = 0);
   } else if (nav == 0) {
     document.getElementById("mobNavLinks").style.display = "none";
+
+    if (cssLink) {
+      document.head.removeChild(cssLink);
+    }
     return (nav = 1);
   }
 }
@@ -15,4 +30,3 @@ function mobNavToggle() {
 //   }, 1000);
 
 // }
-
